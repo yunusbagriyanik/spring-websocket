96387e9c53f871b536f3527819ca86a69b72b438
yunusbagriyanik <yunusbgrynk@gmail.com>
2022-10-23 21:12:02 +0300

    spring web socket configuration

---
+++ src/main/java/com/yunusbagriyanik/springwebsocket/controller/EventController.java
@@ -0,0 +1,21 @@
+package com.yunusbagriyanik.springwebsocket.controller;
+
+import com.yunusbagriyanik.springwebsocket.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/address")
+    public void onMessage(@Payload Message message) {
+        System.out.println(message.toString());
+        this.messagingTemplate.convertAndSend("/channelHook", message);
+    }
+}
---
+++ src/main/java/com/yunusbagriyanik/springwebsocket/model/Message.java
@@ -0,0 +1,22 @@
+package com.yunusbagriyanik.springwebsocket.model;
+
+public class Message {
+    private String sender;
+    private String message;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
