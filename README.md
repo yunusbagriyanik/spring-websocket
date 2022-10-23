af4eb4ec5960b43e7a6d079407386869ea89795b
yunusbagriyanik <yunusbgrynk@gmail.com>
2022-10-23 22:49:02 +0300

    push message

---
+++ README.md
@@ -0,0 +1,16 @@
+### Spring Boot WebSocket
+
+### Requirements
+
+* JDK 17(openjdk-17)
+* Spring Boot 2.7.5
+* Maven 3.6.3 or newer
+* Spring Web Socket
+
+### Build App
+
+    $ mvn clean install
+
+### Run App
+
+    $ mvn spring-boot:run
--- src/main/java/com/yunusbagriyanik/springwebsocket/controller/EventController.java
+++ src/main/java/com/yunusbagriyanik/springwebsocket/controller/EventController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +19,11 @@ public class EventController {
     public void onMessage(@Payload Message message) {
         System.out.println(message.toString());
         this.messagingTemplate.convertAndSend("/channelHook", message);
+    }
+
+    @PostMapping("/push")
+    public void pushMessage(@RequestBody Message message) {
+        System.out.println(message);
+        messagingTemplate.convertAndSend("/channelHook", message);
     }
 }
